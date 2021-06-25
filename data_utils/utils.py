314116905a7f33ac808e41962b7bd5405fd43052
XinZhaoFu <35882457+XinZhaoFu@users.noreply.github.com>
2021-06-25 11:44:41 +0800

    update

--- data_utils/utils.py
+++ data_utils/utils.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import face_recognition
 import numpy as np
 import os
 import shutil
@@ -172,3 +173,47 @@ def check_file_is_aug(img_file_path_list, label_file_path_list):
             normal_label_file_path_list.append(label_file_path)
 
     return normal_img_file_path_list, normal_label_file_path_list, aug_img_file_path_list, aug_label_file_path_list
+
+
+def face_crop(img):
+    img_rows, img_cols, img_channel = img.shape
+    if img_rows < 512 or img_cols < 512:
+        img = cv2.resize(img, (512, 512))
+
+    filling_img = filling(img)
+    location = face_recognition.face_locations(filling_img)
+    if len(location) == 0:
+        return img
+
+    [(x1, y2, x2, y1)] = location
+    # crop_x1 = x1 - (x2 - x1) // 2
+    # crop_x2 = x2 + (x2 - x1) // 2
+    # crop_y1 = y1 - (y2 - y1) // 2
+    # crop_y2 = y2 + (y2 - y1) // 2
+
+    crop_x1 = max(x1 - (x2 - x1) // 2, 256)
+    crop_x2 = min(x2 + (x2 - x1) // 2, img_rows + 256)
+    crop_y1 = max(y1 - (y2 - y1) // 2, 256)
+    crop_y2 = min(y2 + (y2 - y1) // 2, img_cols + 256)
+    if crop_y2 - crop_y1 > crop_x2 - crop_x1:
+        crop_y2 = (crop_y2 + crop_y1) // 2 + (crop_x2 - crop_x1) // 2
+        crop_y1 = (crop_y2 + crop_y1) // 2 - (crop_x2 - crop_x1) // 2
+    if crop_x2 - crop_x1 > crop_y2 - crop_y1:
+        crop_x2 = (crop_x2 + crop_x1) // 2 + (crop_y2 - crop_y1) // 2
+        crop_x1 = (crop_x2 + crop_x1) // 2 - (crop_y2 - crop_y1) // 2
+
+    img = filling_img[crop_x1:crop_x2, crop_y1:crop_y2, :]
+
+    return img
+
+
+def filling(img):
+    """
+    :param img:
+    :return:
+    """
+    img_rows, img_cols, img_channel = img.shape
+    filling_img = np.zeros(shape=(img_rows + 512, img_cols + 512, 3), dtype=np.uint8)
+    filling_img[256:img_rows+256, 256:img_cols+256] = img[:, :]
+
+    return filling_img
